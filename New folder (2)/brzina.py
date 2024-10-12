fa9d79b787fcc87f5c6425b48d9b2317b8596c51
aleksagasparovski <aleksagasparovski@gmail.com>
2024-10-12 13:09:10 +0200

    poruka

---
+++ New folder (2)/brzina.py
@@ -0,0 +1,5 @@
+def brzina(vreme1, put1, vreme2, put2, vreme3, put3):
+    ukupna_udaljenost = put1+put2+put3
+    ukupno_vreme=vreme1+vreme2+vreme3
+    prosecna_brzina = ukupna_udaljenost/ukupno_vreme
+    return prosecna_brzina
---
+++ New folder (2)/main.py
@@ -0,0 +1,10 @@
+from brzina import brzina
+vreme1=0.5
+put1=1
+vreme2=2
+put2=6
+vreme3=2
+put3=5
+
+prosecna = brzina(put1,vreme1,put2,vreme2,put3,vreme3)
+print("prosecna brzina je", prosecna)
